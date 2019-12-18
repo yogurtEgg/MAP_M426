be621efea31157a54dc797811ba91df66d5727fa
senti <senti.noelle@gmail.com>
2019-12-18 21:07:59 +0100

    Root Controller

---
+++ AddressMap/src/main/java/ch/schule/AddressApp/RootController.java
@@ -0,0 +1,12 @@
+package ch.schule.AddressApp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RootController {
+    @FXML
+    public void handleButtonClose(ActionEvent actionEvent) {
+        System.out.println("Test erfolgreich");
+    }
+}
+
